ec660bd3379ab3e84ca55d7aaea2740ce5528bc6
doc-gif_mac_mini <i.h.business118@gmail.com>
2026-03-02 16:37:41 +0900

    サンプルデータの追加バナーを削除

--- src/presentation/pages/HomePage.tsx
+++ src/presentation/pages/HomePage.tsx
@@ -1,11 +1,9 @@
 import { InventoryList } from "@/presentation/components/InventoryList";
 import React from "react";
-import {SeedDataBanner} from "@/presentation/pages/SeedData";
 
 export function HomePage() {
   return (
       <>
-        <SeedDataBanner />
         <InventoryList />
       </>
   );
